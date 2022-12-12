1523286eeed8dc1672dcada94d2ab39ed1841148
popularbelbase07 <popularbelbase92@gmail.com>
2022-12-12 13:50:15 +0100

    creating environment variables for connecting backend   .env.development         .env.production         src/endpoints.ts

--- src/genres/IndexGenres.tsx
+++ src/genres/IndexGenres.tsx
@@ -1,13 +1,15 @@
 import axios, { AxiosResponse } from "axios";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { urlGenres } from "../endpoints";
 import { genreDTO } from "./Genres.model";
 
 export default function IndexGenres(){
+     
 // using react hook and axios to communicate with the backend services
-const GenreURL ="https://localhost:7246/api/Genres";
+//const GenreURL ="https://localhost:7246/api/Genres";
 useEffect(() => {
-    axios.get(GenreURL)
+    axios.get(urlGenres)
     .then((response: AxiosResponse<genreDTO[]>) => {
         console.log(response.data)
     })
