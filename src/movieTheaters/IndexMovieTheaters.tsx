844447be3b07e376106d61af2e349435534a4f6c
popularbelbase07 <popularbelbase92@gmail.com>
2022-12-20 00:14:24 +0100

    done with displaying the created movies in MovieTheaters

--- src/movieTheaters/IndexMovieTheaters.tsx
+++ src/movieTheaters/IndexMovieTheaters.tsx
@@ -1,10 +1,34 @@
 import { Link } from "react-router-dom";
+import { urlMovieTheaters } from "../endpoints";
+import IndexEntity from "../Utils/Resuable Component/IndexEntity";
+import { movieTheaterDTO } from "./MovieTheater.model";
 
 export default function MovieTheaters(){
     return(
-        <>
-        <h3>Movie Theaters</h3>
-        <Link className="btn btn-primary"  to= "/movietheaters/create">Create Movie Theater</Link>
-        </>
+        <IndexEntity<movieTheaterDTO>
+        url={urlMovieTheaters}
+        createUrl="movieTheater/create"
+        title="Movie Theaters"
+        entityName="Movie Theater"
+      >
+        {(movieTheaters, buttons) =>
+          <>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {movieTheaters?.map((movieTheater) =>
+                <tr key={movieTheater.id}>
+                  <td>{buttons(`movieTheaters/edit/${movieTheater.id}`, movieTheater.id)}</td>
+                  <td>{movieTheater.name}</td>
+                </tr>
+              )}
+            </tbody>
+          </>
+        }
+      </IndexEntity>
     )
 }
